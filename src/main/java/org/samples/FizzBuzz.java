0599a93c08a53b2381330ba3aaa662dea3354568
Diana Sherwood <diana@5000Solutions.com>
2025-05-18 11:49:51 -0700

    - r Extract divisibility checks into helper methods in FizzBuzz.java

--- src/main/java/org/samples/FizzBuzz.java
+++ src/main/java/org/samples/FizzBuzz.java
@@ -3,15 +3,27 @@ package org.samples;
 public class FizzBuzz {
 
     public static String of(int n) {
-        if (n % 3 == 0 && n % 5 == 0) {
+        if (isDivisibleBy15(n)) {
             return "FizzBuzz";
         }
-        if (n % 3 == 0) {
+        if (isDivisibleBy3(n)) {
             return "Fizz";
         }
-        if (n % 5 == 0) {
+        if (isDivisibleBy5(n)) {
             return "Buzz";
         }
         return String.valueOf(n);
+    }
+
+    private static boolean isDivisibleBy3(int n) {
+        return n % 3 == 0;
+    }
+
+    private static boolean isDivisibleBy5(int n) {
+        return n % 5 == 0;
+    }
+
+    private static boolean isDivisibleBy15(int n) {
+        return n % 15 == 0;
     }
 }
