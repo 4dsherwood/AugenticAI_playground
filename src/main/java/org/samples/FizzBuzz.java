aa6a0b4c08cf64927974f03aa75eccbd599b7f40
Diana Sherwood <diana@5000Solutions.com>
2025-05-18 11:52:04 -0700

    - r Refactor FizzBuzz.java to use single isDivisibleBy helper method

--- src/main/java/org/samples/FizzBuzz.java
+++ src/main/java/org/samples/FizzBuzz.java
@@ -3,27 +3,19 @@ package org.samples;
 public class FizzBuzz {
 
     public static String of(int n) {
-        if (isDivisibleBy15(n)) {
+        if (isDivisibleBy(n, 15)) {
             return "FizzBuzz";
         }
-        if (isDivisibleBy3(n)) {
+        if (isDivisibleBy(n, 3)) {
             return "Fizz";
         }
-        if (isDivisibleBy5(n)) {
+        if (isDivisibleBy(n, 5)) {
             return "Buzz";
         }
         return String.valueOf(n);
     }
 
-    private static boolean isDivisibleBy3(int n) {
-        return n % 3 == 0;
-    }
-
-    private static boolean isDivisibleBy5(int n) {
-        return n % 5 == 0;
-    }
-
-    private static boolean isDivisibleBy15(int n) {
-        return n % 15 == 0;
+    private static boolean isDivisibleBy(int n, int divisor) {
+        return n % divisor == 0;
     }
 }
